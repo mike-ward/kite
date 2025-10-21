77643dbb3489cbbc65da506e7f81a28e696061e6
mike-ward <mike@mike-ward.net>
2025-10-21 11:33:51 -0500

    refactor: update text styles and improve timeline view UI
    - Centralize text style definitions and improve reusability
    - Add hover cursor handling for image posts
    - Introduce padding and alignment adjustments for better visuals
    - Refine sanitization and truncation logic for post details
    - Replace deprecated image width constant with in-line styling

--- kite.v
+++ kite.v
@@ -2,9 +2,6 @@ import gui
 
 pub const app_min_width = 300
 pub const app_height = 900
-pub const image_width = 250
-pub const max_image_height = 250
-pub const max_timeline_posts = 25
 
 @[heap]
 struct KiteApp {
--- timeline_view.v
+++ timeline_view.v
@@ -1,22 +1,12 @@
 import gui
 import os
 
+const image_width = 270
+const max_image_height = 260
+const max_timeline_posts = 25
 const thin_space = '\xE2\x80\x89'
 const link_color = gui.cornflower_blue
 const post_text_color = gui.rgb(160, 160, 160)
-const post_text_style = gui.TextStyle{
-	...gui.theme().text_style
-	color: post_text_color
-}
-const post_link_style = gui.TextStyle{
-	...gui.theme().text_style
-	color: link_color
-}
-const post_repost_style = gui.TextStyle{
-	...gui.theme().text_style
-	color: post_text_color
-	size:  gui.theme().size_text_small
-}
 
 fn timeline_view(window &gui.Window) gui.View {
 	app := kite_app(window)
@@ -31,39 +21,71 @@ fn timeline_view(window &gui.Window) gui.View {
 			content: [gui.text(text: 'Fetching Timeline...', text_style: gui.theme().b1)]
 		)
 	} else {
+		base_text_style := gui.theme().n2
+		post_text_style := gui.TextStyle{
+			...base_text_style
+			color: post_text_color
+		}
+		post_link_style := gui.TextStyle{
+			...base_text_style
+			size:  base_text_style.size - 1
+			color: link_color
+		}
+		post_repost_style := gui.TextStyle{
+			...base_text_style
+			color: post_text_color
+			size:  base_text_style.size - 1
+		}
+
 		for post in app.timeline.posts {
 			post_author := sanitize_text(post.author)
 			post_text := sanitize_text(post.text)
 			if post_author.is_blank() || post_text.is_blank() {
 				continue
 			}
+
+			mut post_content := []gui.View{cap: 10}
+
 			time_short := post.created_at
 				.utc_to_local()
 				.relative_short()
 				.fields()[0]
 			timestamp := if time_short == '0m' { 'now' } else { time_short }
 			author_timestamp := truncate_long_fields('${post_author} • ${timestamp}')
-			mut post_content := []gui.View{cap: 6}
 
 			if !post.repost_by.is_blank() {
+				reposted_by := truncate_long_fields('•${thin_space}reposted by ${remove_non_ascii(post.repost_by)}')
 				post_content << gui.text(
-					text:       truncate_long_fields('•${thin_space}reposted by ${remove_non_ascii(post.repost_by)}')
+					text:       reposted_by
 					text_style: post_repost_style
 				)
 			}
-			post_content << gui.text(text: author_timestamp)
-			post_content << text_link(post_text, post.bsky_link_uri, post_text_style)
+
+			post_content << text_link(author_timestamp, post.bsky_link_uri, base_text_style)
+
+			post_content << gui.text(
+				text:       sanitize_text(post.text)
+				mode:       .wrap
+				text_style: post_text_style
+			)
+
 			if !post.link_uri.is_blank() {
-				post_content << text_link(post.link_title, post.link_uri, post_link_style)
+				post_content << gui.rectangle(height: gui.pad_x_small) // spacer
+				post_content << text_link(sanitize_text(post.link_title), post.link_uri,
+					post_link_style)
 			}
+
 			if !post.image_path.is_blank() {
 				post_content << gui.column(
 					h_align: .center
+					padding: gui.Padding{
+						top:   gui.pad_small
+						right: gui.pad_small
+					}
 					sizing:  gui.fill_fit
 					content: [
 						gui.image(
 							file_name:  post.image_path
-							width:      image_width
 							max_height: max_image_height
 							on_click:   fn [post] (_ &gui.ImageCfg, mut e gui.Event, mut _ gui.Window) {
 								e.is_handled = true
@@ -71,17 +93,23 @@ fn timeline_view(window &gui.Window) gui.View {
 									print_error(err.msg(), @FILE_LINE)
 								}
 							}
+							on_hover:   fn (mut _ gui.Layout, mut e gui.Event, mut w gui.Window) {
+								w.set_mouse_cursor_pointing_hand()
+								e.is_handled = true
+							}
 						),
 					]
 				)
 			}
+
 			post_content << gui.rectangle(height: gui.pad_small) // spacer
-			post_content << gui.rectangle(
+			post_content << gui.rectangle( // divider line
 				height: 0.4
 				width:  w - gui.pad_large - gui.pad_medium
 				sizing: gui.fixed_fixed
 				color:  post_text_color
 			)
+
 			content << gui.column(
 				padding: gui.padding_none
 				sizing:  gui.fill_fit
@@ -91,25 +119,24 @@ fn timeline_view(window &gui.Window) gui.View {
 		}
 	}
 	return gui.column(
-		id_scroll: 1
-		width:     w
-		height:    h
-		sizing:    gui.fixed_fixed
-		padding:   gui.Padding{
+		id_scroll:   1
+		scroll_mode: .vertical_only
+		width:       w
+		height:      h
+		sizing:      gui.fixed_fixed
+		padding:     gui.Padding{
 			top:    gui.pad_x_small
 			bottom: gui.pad_small
 			left:   gui.pad_small + gui.pad_x_small
 			right:  gui.pad_medium
 		}
-		content:   content
+		content:     content
 	)
 }
 
 fn text_link(link_title string, link_uri string, text_style gui.TextStyle) gui.View {
 	return gui.column(
-		padding:  gui.Padding{
-			top: gui.pad_small
-		}
+		padding:  gui.padding_none
 		sizing:   gui.fill_fit
 		on_click: fn [link_uri, link_title] (_ voidptr, mut e gui.Event, mut _ gui.Window) {
 			e.is_handled = true
@@ -121,7 +148,7 @@ fn text_link(link_title string, link_uri string, text_style gui.TextStyle) gui.V
 		}
 		content:  [
 			gui.text(
-				text:       sanitize_text(link_title)
+				text:       link_title
 				mode:       .wrap
 				text_style: text_style
 			),
